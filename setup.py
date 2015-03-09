97cbdc87246398448860e9e77331911bcab291e1
Sergio E <ark.sega@gmail.com>
2015-03-08 19:26:42 -0600

    Agregando sertup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='Admin-CFDI',
+      version='0.2.6',
+      description='Herramienta para administracion de CFDIs',
+      license='GPL 3.0',
+      author='Mauricio Baeza',
+      author_email='correopublico@mauriciobaeza.org',
+      url='https://facturalibre.net/servicios/',
+      py_modules=['pyutil', 'values', 'template'],
+      scripts=['admincfdi.py'])
+
+# vim: ts=4 et sw=4
