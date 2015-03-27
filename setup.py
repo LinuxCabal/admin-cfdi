846375d24affa52ded711d5626871f8fbb8e15ad
Sergio E <ark.sega@gmail.com>
2015-03-27 02:10:07 -0600

    Actualizar a 0.2.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 from setuptools import setup, find_packages
 
 setup(name='Admin-CFDI',
-      version='0.2.6',
+      version='0.2.7',
       description='Herramienta para administracion de CFDIs',
       license='GPL 3.0',
       author='Mauricio Baeza',
