b1bb12890bb57093cae338445dc9a0ce5b4352bb
Mauricio Baeza <web@mauriciobaeza.net>
2015-03-29 23:08:59 -0600

    Cambiado los derechos de Mauricio Baeza a Python Cabal

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'admin-cfdi'
-copyright = '2015, Mauricio Baeza'
+copyright = '2015, Python Cabal'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
