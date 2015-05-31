4b17062fa695c4933f9fcbc417c38b423a2eef92
Patricio Paez <pp@pp.com.mx>
2015-05-31 10:57:37 -0500

    Corregir error por codepage 437 en consola
    - Queda pendiente arreglar acentos y ñ

--- cfdi2pdf.cmd
+++ cfdi2pdf.cmd
@@ -1,3 +1,4 @@
 @echo off
+set PYTHONIOENCODING=utf-8
 set c2p_path="%~d0\%~p0cfdi2pdf"
 py %c2p_path% %*
--- descarga-cfdi.cmd
+++ descarga-cfdi.cmd
@@ -1,3 +1,4 @@
 @echo off
+set PYTHONIOENCODING=utf-8
 set dc_path="%~d0\%~p0descarga-cfdi"
 py %dc_path% %*
