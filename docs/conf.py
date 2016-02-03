d681142fea77bf59fe19e831587db26b80f41036
Mauricio Baeza <web@mauriciobaeza.net>
2016-02-02 23:15:35 -0600

    Quitar acento en html_title en conf.py para que se genere correctamente la documentación

--- docs/conf.py
+++ docs/conf.py
@@ -111,7 +111,7 @@ html_theme = 'default'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = "Documentación de {} v{}".format(project, release)
+html_title = "Documentacion de {} v{}".format(project, release)
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 html_short_title = html_title
